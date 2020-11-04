8b7dc68f53d55a783e42561cceb9323988249d62
Magicrafter13 <scubaventure101@hotmail.com>
2020-11-03 22:44:23 -0800

    Initial version
    Can't wait to get this off the ground.

---
+++ README.md
@@ -0,0 +1,7 @@
+# stembot
+Discord bot to help automate tasks like managing roles.
+
+Intended only for use on the Student-run Clark College STEM Discord server.
+
+
+### **NOT FOR PUBLIC VIEWING**
---
+++ index.js
@@ -0,0 +1,20 @@
+// Private config file
+const config = require('./config.json');
+// Discord.js library - create Discord object
+const Discord = require('discord.js');
+// Register Discord client
+const client = new Discord.Client();
+
+// Execute when client ready.
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('Pong!');
+	}
+});
+
+// Login with token
+client.login(config.token);
