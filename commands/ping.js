a6b23dcd5e210f6ebff436055cf17f2cfeb2444e
nspelts <speltsn@gmail.com>
2020-11-04 23:04:40 -0800

    Fixed undefined command name, cooldown default fix ready for merge

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
+	cooldown: 5,
 	execute(message) {
         message.channel.send(`Pong! This message had a latency of ${Date.now() - message.createdTimestamp}ms.`);
 	},
--- index.js
+++ index.js
@@ -39,14 +39,14 @@ client.on('message', message => {
 
 	const now = Date.now();
 	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
+	const cooldownAmount = (client.commands.get(command).cooldown || 0) * 1000;
 
 	if (timestamps.has(message.author.id)) {
 		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
 		if (now < expirationTime) {
 			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the ${command} command.`);
 		}
 	}
 
