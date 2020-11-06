837bb66cf0b73ff98002796c5fdf9711708d51a4
Magicrafter13 <scubaventure101@hotmail.com>
2020-11-05 17:17:41 -0800

    Added command framework
    See ping.js for updated command variable requirements.

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,12 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
+	guildOnly: false,
 	cooldown: 5,
-	execute(message) {
+	argsMin: 0,
+	argsMax: -1,
+	usage: '',
+	execute(message, args) {
         message.channel.send(`Pong! This message had a latency of ${Date.now() - message.createdTimestamp}ms.`);
 	},
 };
--- index.js
+++ index.js
@@ -20,6 +20,7 @@ for (const file of commandFiles) {
 }
 
 const cooldowns = new Discord.Collection();
+const permWhitelist = ['ADMINISTRATOR']; // Users with these permissions will not be subject to the cooldown.
 
 // Execute first time ready event is received only
 client.once('ready', () => {
@@ -31,9 +32,26 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return; // checks for prefix
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/); // looks for arguments and assigns them
-	const command = args.shift().toLowerCase(); // takes command and makes it lowercase/assigns it to variable
+	const commandName = args.shift().toLowerCase(); // takes command and makes it lowercase/assigns it to variable
 
-	if (!client.commands.has(command)) return;
+	// Recommend the cooldown code is moved inside the try-catch area, just to be safe
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+
+	if (!command) return; // No command called 'commandName' exists
+
+	if (command.guildOnly && message.channel.type === 'dm')
+		return message.reply('This command cannot be used in a DM.');
+
+	/*
+	 * May not actually go this route, as I plan to make some more complex commands, and I feel it may be
+	 * easier to handle the usage output from the command file. At the very least, just have the execute
+	 * function return different values (error codes), and then we can call upon command.usage based on
+	 * what execute returns.
+	 */
+	if (command.argsMax != -1 && (args.length < command.argsMin || args.length > command.argsMax))
+		return message.channel.send(`Invalid command syntax, usage:\n\`\`\`\n${command.name} ${command.usage}\n\`\`\``)
 
 	if (!cooldowns.has(command.name)) {
 		cooldowns.set(command.name, new Discord.Collection());
@@ -41,14 +59,16 @@ client.on('message', message => {
 
 	const now = Date.now();
 	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (client.commands.get(command).cooldown || 0) * 1000;
+	const cooldownAmount = (command.cooldown || 0) * 1000;
 
 	if (timestamps.has(message.author.id)) {
 		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the ${command} command.`);
+		if (message.channel.type !== 'dm' && !message.member.permissions.any(permWhitelist)) {
+			if (now < expirationTime) {
+				const timeLeft = (expirationTime - now) / 1000;
+				return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the ${command.name} command.`);
+			}
 		}
 	}
 
@@ -56,9 +76,9 @@ client.on('message', message => {
 	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 	try {
-		client.commands.get(command).execute(message, args); // attempts to execute command
+		command.execute(message, args); // attempts to execute command
 	} catch (error) {
-		console.error(error); // error mesage for console
+		console.error(error);
 		message.reply('there was an error trying to execute that command!'); // error message for user
 	}
 });
