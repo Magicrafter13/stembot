09d31b81f460c5fb431802d5c10dc7908693d5e8
Matthew <scubaventure101@hotmail.com>
2020-11-23 21:28:01 -0800

    Basically finished

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,15 @@
 module.exports = {
 	name: 'ping',
-	description: 'Ping!',
+	description: 'Gives pseudo-accurate latency info.',
 	guildOnly: false,
 	cooldown: 5,
 	argsMin: 0,
 	argsMax: -1,
 	usage: '',
 	execute(message, args) {
-        message.channel.send(`Pong! This message had a latency of ${Date.now() - message.createdTimestamp}ms.`);
+		message.channel.send(`Pong! This message had a latency of ${Date.now() - message.createdTimestamp}ms.`);
+	},
+	help(prefix) {
+		return `${prefix}ping`;
 	},
 };
