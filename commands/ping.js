971151fd568e2e15a1feb1ef267d69106d54a89f
Matthew <scubaventure101@hotmail.com>
2020-11-23 23:10:02 -0800

    Remove old usage variable.

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,6 @@ module.exports = {
 	cooldown: 5,
 	argsMin: 0,
 	argsMax: -1,
-	usage: '',
 	execute(message, args) {
 		message.channel.send(`Pong! This message had a latency of ${Date.now() - message.createdTimestamp}ms.`);
 	},
