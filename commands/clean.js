35cd777c715a93defba71e662d0aa017f5f15f68
Matthew <scubaventure101@hotmail.com>
2020-11-23 22:21:58 -0800

    clean fully integrated with database
    now only accepts one argument (-b/--bot)

--- commands/clean.js
+++ commands/clean.js
@@ -1,20 +1,20 @@
-function purgeRoles(message, roles, masterRole, subRoles) {
-	if (roles.has(masterRole)) {
-		if (subRoles.length > 0) {
-			message.guild.members.cache.each(member => {
-				if (!member.roles.cache.has(masterRole)) {
-					subRoles.forEach(roleKey => {
-						if (member.roles.cache.has(roleKey)) {
-							message.channel.send(`Removing ${roles.get(roleKey).name} from ${member.displayName}.`);
-							member.roles.remove(roles.get(roleKey), `${message.author.username} requested role clean for users without ${roles.get(masterRole).name}.`);
-						}
-					});
-				}
-			});
-		}
-		else message.channel.send('Please specify at least one more argument.');
-	}
-	else message.channel.send('That role doesn\'t exist.');
+function purgeRoles(message, roles, masterID, subIDs) {
+	roles.fetch(masterID)
+		.then(masterRole => {
+			subIDs.forEach(id => {
+				roles.fetch(id)
+					.then(role => {
+						role.members.each(member => {
+							if (!member.roles.cache.has(masterID)) {
+								message.channel.send(`Removing ${role.name} from ${member.displayName}.`);
+								member.roles.remove(id, `${message.author.username} requested role clean. User did not have ${masterRole.name}.`);
+							}
+						});
+					})
+				.catch(console.error);
+			})
+		})
+	.catch(console.error);
 }
 
 module.exports = {
@@ -22,8 +22,8 @@ module.exports = {
 	description: 'Cleans bad roles from users',
 	guildOnly: true,
 	cooldown: 0,
-	argsMin: 1,
-	argsMax: -1,
+	argsMin: 0,
+	argsMax: 1,
 	usage: '-b|--bot\n(or:) <required role> <sub-role 1> ...',
 	execute(message, args, settings) {
 		// Check for authorization
@@ -70,45 +70,20 @@ module.exports = {
 			return;
 		}
 
-		// Handle regex flag
-		let regex = args[0] === '-r' || args[0] === '--regex';
-		if (regex) {
-			args.shift();
-			if (args.length < 2) return message.channel.send(`Expecting 3 arguments, only got ${args.length + 1}.`);
-		}
-
 		// Any person without <required role> will have all sub-role n removed
-		const roles = message.guild.roles.cache;
-		const masterRole = roles.findKey(role => role.toString() === args[0]);
-		args.shift();
-		if (regex) {
-			// Get confirmation from at least one other Admin (unless command issued by server owner)
-			if (message.member !== message.guild.owner) {
-				let auth = false;
-				message.channel.send("Purging roles via Regular Expression requires approval of at least one more authorized user.\nIf you have the MANAGE_ROLES permission, and you approve of this action, please say `approve`.").then(() => {
-					// This is ugly, not sure of proper formatting
-					const filter = m => m.member.hasPermission('MANAGE_ROLES', {
-						checkAdmin: true,
-						checkOwner: true,
-					}) && m.author !== message.author && m.content === 'approve';
+		const roles = message.guild.roles;
+		const catDB = settings.get('categories');
+		catDB.get(message.guild.id)
+			.then(categories => {
+				if (categories === undefined)
+					return message.channel.send(`No class information exists, set class categories with \`catman\`.`);
 
-					const approve = message.channel.createMessageCollector(filter, { time: 60000, max: 1, errors: ['time'] });
-					approve.on('collect', messages => {
-						message.channel.send('Authorization granted, continuing command execution.');
-						auth = true;
-						const re = new RegExp(args[0]);
-						const subRoles = roles.filter(role => re.test(role.name)).map(val => roles.findKey(role => val === role));
-						purgeRoles(message, roles, masterRole, subRoles);
-					});
-					approve.on('end', () => {
-						if (!auth) message.channel.send('Authorization not received within acceptable timeframe, aborting command.');
-					});
-				});
-			}
-		}
-		else {
-			const subRoles = args.map(arg => roles.findKey(role => role.toString() === arg));
-			purgeRoles(message, roles, masterRole, subRoles);
-		}
+				categories.forEach(catData => purgeRoles(message, roles, catData.id, catData.roles));
+			})
+		.catch(console.error);
+		/*const masterRole = roles.findKey(role => role.toString() === args[0]);
+		args.shift();
+		const subRoles = args.map(arg => roles.findKey(role => role.toString() === arg));
+		purgeRoles(message, roles, masterRole, subRoles);*/
 	},
 };
