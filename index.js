17efd48b217cea85218185b13ab2b71c9627b616
Matthew <scubaventure101@hotmail.com>
2021-02-05 17:34:06 -0800

    Tried, but
    Unfortunately #13 may be impossible. Was worried about this but can't
    say I'm surprised. Somewhat surprised we can even remove other users'
    reactions, so it makes sense that changing/adding them is off limits.

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ client.on('messageReactionRemove', async (reaction, user) => {
 						// Remove reactions to class react-role message
 						reaction.message.guild.channels.resolve(thing.reactor.channel)
 						.messages.resolve(thing.reactor.message)
-							.reactions.cache.forEach(async function (class_reaction) { await class_reaction.users.remove(member); }); // Is this actually any different than before? Might be a waste of code...
+							.reactions.cache.forEach(class_reaction => class_reaction.users.remove(member)); // Is this actually any different than before? Might be a waste of code...
 					}
 				})
 			.catch(console.error)
